14e720246356b778d69aacca3c19b5ad2a77a7ff
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-06-14 15:13:50 +0700

    Update 6_ℹ︎ABOUT.py

--- pages/6_ℹ︎ABOUT.py
+++ pages/6_ℹ︎ABOUT.py
@@ -4,11 +4,6 @@ import streamlit.components.v1 as components
 # Set page configuration
 st.set_page_config(page_title="Dashboard", page_icon="📈", layout="wide") 
 
-
-# Define a function for the tooltip (if needed)
-def tooltip(image_url, text):
-    return f'<img src="{image_url}" title="{text}" style="border: 1px solid blue; border-radius:4px; background-color:green; color: white; padding:2px;">'
-
 # HTML content
 html_content = f"""
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -19,7 +14,8 @@ html_content = f"""
     html {{
         margin-top: 0px;
         margin-bottom: 0px;
-        border: 1px solid #de3f53; padding:0px 4px;
+        border: 1px solid #de3f53;
+        padding: 0px 4px;
         font-family: "Source Sans Pro", sans-serif;
         font-weight: 400;
         line-height: 1.6;
@@ -31,7 +27,7 @@ html_content = f"""
     }}
     </style>
 
-    <h2 style='color: #de3f53; margin-top:0px; border-bottom: solid 5px;'>Tentang Analisis Klaster AHC dan K-Means pada Data BPS Kabupaten Purwakarta</h2>
+    <h2 style='color: #de3f53; margin-top: 0px; border-bottom: solid 5px;'>Tentang Analisis Klaster AHC dan K-Means pada Data BPS Kabupaten Purwakarta</h2>
 """
 
 # Display the HTML content
@@ -40,8 +36,7 @@ st.markdown(html_content, unsafe_allow_html=True)
 # Menambahkan jarak
 st.markdown("&nbsp;", unsafe_allow_html=True)  # Spasi menggunakan HTML entity
 
-
- # Deskripsi menggunakan markdown
+# Deskripsi menggunakan markdown
 st.success('''
         **Pendahuluan: 📊**
         Pertumbuhan penduduk yang dipengaruhi oleh faktor alam, migrasi, dan kebijakan menjadikan Kabupaten Purwakarta, Jawa Barat, sebagai Kota Metropolitan pada tahun 2023 dengan jumlah penduduk 1.028.569 jiwa dan luas wilayah 971,72 km². Kepadatan penduduk mencerminkan distribusi penduduk di wilayah tersebut. Pertumbuhan tahunan mencapai 15.774 jiwa (1,52%), dengan Kecamatan Campaka memiliki pertumbuhan tertinggi (2,92%) dan Kecamatan Purwakarta terendah (0,71%).
@@ -76,7 +71,7 @@ st.success('''
 
         Analisis klaster dengan AHC dan K-Means diharapkan dapat memberikan gambaran yang lebih dalam tentang populasi Kabupaten Purwakarta. Hasilnya dapat digunakan sebagai dasar untuk kebijakan pembangunan yang lebih efektif, memastikan bahwa sumber daya dialokasikan dengan bijak sesuai dengan kebutuhan unik setiap wilayah.
     ''')
- 
+
 st.info('''
         Hasil analisis klaster menggunakan metode Agglomerative Hierarchical Clustering (AHC) dan K-Means pada data demografis BPS Kabupaten Purwakarta dapat memberikan wawasan yang berharga untuk:
 
@@ -94,37 +89,36 @@ st.info('''
             Analisis ini memberikan landasan untuk **menginformasikan** kebijakan pembangunan wilayah dan **mengeksplorasi** faktor-faktor yang dapat mempengaruhi pola demografis di Kabupaten Purwakarta.
             ''', icon="🧐")
 
+# Container for YouTube video about K-Means Clustering
 with st.container(border=True):
-        st.markdown("""
-        Berikut adalah video YouTube yang menjelaskan tentang K-Means Clustering. Dapat di akses dan menonton video ini langsung di YouTube untuk mendapatkan pemahaman yang lebih baik tentang metode tersebut.
-        """)
+    st.markdown("""
+        Berikut adalah video YouTube yang menjelaskan tentang K-Means Clustering. Anda dapat menonton video ini langsung di YouTube untuk memperoleh pemahaman yang lebih baik tentang metode tersebut.
+    """)
 
-        # Embed YouTube video
-        video_url_kmeans = "https://www.youtube.com/embed/BMzXuG1p3lQ?t=887s"
-        video_html_kmeans = f"""
-            <div style="display: flex; justify-content: center;">
-                <iframe width="1000" height="450" src="{video_url_kmeans}" 
-                frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                </iframe>
-            </div>
-        """
-        components.html(video_html_kmeans, height=450)
-        
+    # Embed YouTube video for K-Means Clustering
+    video_url_kmeans = "https://www.youtube.com/embed/BMzXuG1p3lQ?t=887s"
+    video_html_kmeans = f"""
+        <div style="display: flex; justify-content: center;">
+            <iframe width="1000" height="450" src="{video_url_kmeans}" 
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+        </div>
+    """
+    components.html(video_html_kmeans, height=450)
+
+# Container for YouTube video about Agglomerative Hierarchical Clustering (AHC)
 with st.container(border=True):
-        # Judul dan deskripsi
-        st.markdown("""
-        Berikut adalah video YouTube yang menjelaskan tentang Agglomerative Hierarchical Clustering (AHC). dapat di akses dan menonton video ini langsung di YouTube untuk mendapatkan pemahaman yang lebih baik tentang metode tersebut.
-        """)
+    st.markdown("""
+        Berikut adalah video YouTube yang menjelaskan tentang Agglomerative Hierarchical Clustering (AHC). Anda dapat menonton video ini langsung di YouTube untuk memperoleh pemahaman yang lebih baik tentang metode tersebut.
+    """)
 
-        # Embed YouTube video
-        video_url_ahc = "https://www.youtube.com/embed/s8K0lO9OFOA?start=1067"
-        video_html_ahc = f"""
-            <div style="display: flex; justify-content: center;">
-                <iframe width="1000" height="450" src="{video_url_ahc}" 
-                frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                </iframe>
-            </div>
-        """
-        components.html(video_html_ahc, height=450)
-
-
+    # Embed YouTube video for Agglomerative Hierarchical Clustering (AHC)
+    video_url_ahc = "https://www.youtube.com/embed/s8K0lO9OFOA?start=1067"
+    video_html_ahc = f"""
+        <div style="display: flex; justify-content: center;">
+            <iframe width="1000" height="450" src="{video_url_ahc}" 
+            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+        </div>
+    """
+    components.html(video_html_ahc, height=450)
